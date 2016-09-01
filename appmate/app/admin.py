c86e376299c0e4e09d0216d45f699ffa0ce3a39b
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-01 13:13:58 +0800

    [models] add sample class

--- appmate/app/admin.py
+++ appmate/app/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 
-from app.models import AppData
+from app.models import Sample
 
 
-@admin.register(AppData)
-class AppDataAdmin(admin.ModelAdmin):
-    pass
+@admin.register(Sample)
+class SampleAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'name',
+        'msg',
+        'date',
+        'timestamp',
+        'img',
+        'checked',
+        'email',
+        'index')
--- appmate/app/models.py
+++ appmate/app/models.py
@@ -1,5 +1,12 @@
 from django.db import models
 
 
-class AppData(models.Model):
-    pass
+class Sample(models.Model):
+    name = models.CharField(max_length=30, blank=True, null=True)
+    msg = models.TextField(blank=True, null=True)
+    date = models.DateField(blank=True, null=True, auto_now=True)
+    timestamp = models.DateTimeField(blank=True, null=True, auto_now=True)
+    img = models.ImageField(blank=True, null=True)
+    checked = models.BooleanField(blank=True)
+    email = models.EmailField(blank=True, null=True)
+    index = models.IntegerField(blank=True, null=True)
