9de7ff7ea1178e09d45515c1a7f0aaa42ca6d287
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-01 19:17:49 +0800

    [app] add basic filter for sample

--- appmate/app/views.py
+++ appmate/app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
-from rest_framework import viewsets
+from rest_framework import filters, viewsets
 
-from app.models import Sample
+from app.models import Sample, SAMPLE_FIELDS
 from app.serializers import SampleSerializer
+
+SAMPLE_FILTER_FIELDS = list(SAMPLE_FIELDS)
+SAMPLE_FILTER_FIELDS.remove('img')
 
 
 class SampleViewSet(viewsets.ModelViewSet):
     queryset = Sample.objects.all()
     serializer_class = SampleSerializer
+    filter_backends = (filters.DjangoFilterBackend,)
+    filter_fields = SAMPLE_FILTER_FIELDS
