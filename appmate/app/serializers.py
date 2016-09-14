5eb274e8cee5682f5cc4fd2e1fb139a484c87137
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-09-15 01:17:18 +0800

    [templ] import serializers.py.jinja

--- appmate/app/serializers.py
+++
@@ -1,9 +0,0 @@
-from rest_framework import serializers
-
-from app.models import Sample, SAMPLE_FIELDS
-
-
-class SampleSerializer(serializers.HyperlinkedModelSerializer):
-    class Meta:
-        model = Sample
-        fields = SAMPLE_FIELDS
