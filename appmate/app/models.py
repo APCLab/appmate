c6864b8747220b297655c2ef05ee92ca212508e8
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-10-02 18:44:40 +0800

    [app] apply template changes

--- appmate/app/models.py
+++ appmate/app/models.py
@@ -11,6 +11,9 @@ class _UtilMixin(object):
     def _fields(cls):
         return tuple(f.name for f in cls._meta.fields)
 
+    def __str__(self):
+        return 'id: {}'.format(self.id)
+
 
 class Sample(_UtilMixin, models.Model):
     name = models.CharField(max_length=42)
