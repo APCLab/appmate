9d4e7ede69e309876f6097b498a526dbcc4be51a
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-10-02 18:48:14 +0800

    [app] apply __str__

--- appmate/app/models.py
+++ appmate/app/models.py
@@ -13,6 +13,9 @@ class _UtilMixin(object):
     def _fields(cls):
         return tuple(f.name for f in cls._meta.fields)
 
+    def __str__(self):
+        return 'id: {}'.format(self.id)
+
 
 class User(_UtilMixin, models.Model):
     iemi = models.CharField(max_length=255, blank=True, null=True)
