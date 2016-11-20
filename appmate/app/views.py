3d96b405706317e0682296ea47a61fba40f243d0
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-11-20 16:02:22 +0800

    [drf] migrate filter

--- appmate/app/views.py
+++ appmate/app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
-from rest_framework import filters, viewsets
+from django_filters.rest_framework import DjangoFilterBackend
+
+from rest_framework import viewsets
 
 from app.filters import *
 from app.models import *
@@ -15,12 +17,12 @@ __all__ = (
 class SampleViewSet(viewsets.ModelViewSet):
     queryset = Sample.objects.all()
     serializer_class = SampleSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filter_class = SampleFilter
 
 
 class DemoViewSet(viewsets.ModelViewSet):
     queryset = Demo.objects.all()
     serializer_class = DemoSerializer
-    filter_backends = (filters.DjangoFilterBackend,)
+    filter_backends = (DjangoFilterBackend,)
     filter_class = DemoFilter
