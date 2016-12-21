1500ded5721e81634274aed3e138cab34eb19cd3
Iblis Lin <iblis@hs.ntnu.edu.tw>
2016-12-21 13:51:56 +0800

    [urls] explicit list STATIC_URL in urlpatterns

--- appmate/core/urls.py
+++ appmate/core/urls.py
@@ -23,4 +23,7 @@ from app.routers import router as app_router
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(app_router.urls)),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
