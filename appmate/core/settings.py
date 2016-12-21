140232b244a8232ba98c11f71068f9ddd24b0eda
Tzu-ting <tzing@outlook.com>
2016-12-21 12:39:21 +0800

    solve issue#2

--- appmate/core/settings.py
+++ appmate/core/settings.py
@@ -125,6 +125,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Django REST Framework
 REST_FRAMEWORK = {
--- appmate/core/urls.py
+++ appmate/core/urls.py
@@ -13,7 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.conf.urls import url, include
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.conf.urls import include, url
+from django.conf.urls.static import static
 from django.contrib import admin
 
 from app.routers import router as app_router
@@ -21,4 +23,6 @@ from app.routers import router as app_router
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(app_router.urls)),
-]
+] \
++ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) \
++ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
