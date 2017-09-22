1cd171e6d95fc59718d74e21f2dbee5cbe807a41
Tzu-ting <tzing@users.noreply.github.com>
2017-09-22 16:28:05 +0800

    Introduce new feature of rest framework 3.6 (#6)
    * support Django REST framework 3.6 feature
    
    * add coreapi minimum requirement version

--- appmate/core/urls.py
+++ appmate/core/urls.py
@@ -18,11 +18,14 @@ from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.contrib import admin
 
+from rest_framework.documentation import include_docs_urls
+
 from app.routers import router as app_router
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include(app_router.urls)),
+    url(r'^docs/', include_docs_urls(title='Appmate Docs')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
